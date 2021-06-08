6ee74139751260947d01fe1dbd05415eb12e6108
farah natsheh <farahnatsheh@hotmail.com>
2021-06-08 14:55:21 -0700

    Add project files.

---
+++ Csharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------First 5 Students List----------");
+            for (int i = 0; i < 5; i++)
+            {
+                students students = new students();
+                Console.WriteLine("Enter Your ID: ");
+                students.ID = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Your Name: ");
+                students.name = Console.ReadLine();
+                Console.WriteLine("Enter Your GPA: ");
+                students.GPA = double.Parse(Console.ReadLine());
+                Console.WriteLine("----------");
+                students.print();
+                Console.WriteLine("***********************");
+            }
+        }
+    }
+    class students
+    {
+        public double ID;
+        public string name;
+        public double GPA;
+      public void print() 
+        {
+            Console.WriteLine("Your ID is: "+ID);
+            Console.WriteLine("Your Name is: "+name);
+            Console.WriteLine("Your GPA is: "+GPA);
+        }
+    }
+}
